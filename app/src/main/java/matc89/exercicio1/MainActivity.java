a59db8644ccb7cd5f507866fec75bec8a4b6d2df
Tassio <tassionoronha@gmail.com>
2019-08-21 20:30:52 -0300

    first commit

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,33 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mensagem;
+    private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.btnCumprimentar);
+        mensagem = (TextView)findViewById(R.id.labelMensagem);
+        editText = (EditText)findViewById(R.id.editNome);
     }
+
+    public void clicou(View v) {
+        String textoDigitado = editText.getText().toString();
+        mensagem.setText(String.format("Olá, %s", textoDigitado));
+    }
+
+
 }
